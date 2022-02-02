1bb07956ab2f55d6ab7d0ef3c1897be84b339ade
Alam Ferrera <alamferrera@gmail.com>
2022-02-02 16:45:51 -0400

    Backend configurado
    se configuro la conexion con la db

---
+++ FBTarjetas/FBTarjetas/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FBTarjetas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FBTarjetas
+{
+    public class ApplicationDbContext:DbContext
+    {
+        DbSet<TarjetaCredito>TarjetaCreditos { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+    }
+}
